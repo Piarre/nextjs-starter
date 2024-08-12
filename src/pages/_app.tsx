28fa06697294c13a6d5289a02d59c26a11be8a35
Piarre <pierre@ideestore.fr>
2024-08-12 14:07:23 +0200

    chore: Update metadata and add Open Graph tags for better SEO and social sharing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GeistSans } from "geist/font/sans";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Head from "next/head";
+import { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "NextJS Starter",
+  description: "A NextJS starter.",
+  keywords: "NextJS, Shadcn UI, Shadcn, Starter, Next, React",
+  openGraph: {
+    url: "https://start.piarre.app/",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +22,32 @@ export default function App({ Component, pageProps }: AppProps) {
       //  max-w-2xl mx-auto py-12 sm:py-24 px-6
       className={cn("min-h-screen bg-background font-sans antialiased", GeistSans.variable)}
     >
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <meta charSet="utf-8" />
+        <meta name="title" content={metadata.title as string} />
+        <meta name="description" content={metadata.description as string} />
+        <meta name="keywords" content="discord, docker, bot, oxie" />
+        <meta name="author" content="Oxie" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content={metadata.title as string} />
+        <meta property="og:description" content={metadata.description as string} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metadata.openGraph?.url as string} />
+
+        {/* Twitter */}
+        {/* <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@Oxie" />
+        <meta name="twitter:title" content="Oxie" />
+        <meta name="twitter:description" content={metadata.description as string} />
+        <meta name="twitter:image" content="https://start.piarre.app/" /> */}
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <ThemeProvider attribute="class" defaultTheme="light">
         <TooltipProvider delayDuration={0}>
           <Component {...pageProps} />
