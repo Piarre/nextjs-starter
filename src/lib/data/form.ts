654d5579003b5adcb0e773e5ade721a56fc6e11c
Piarre <pierre@ideestore.fr>
2024-08-13 11:01:09 +0200

    chore: updated form

--- src/lib/data/form.ts
+++ src/lib/data/form.ts
@@ -3,14 +3,14 @@ import regex from "./regex";
 import { CLIs } from "../types/next";
 
 export const NextStarterFormSchema = z.object({
-  name: z.string().regex(regex.name, {
+  name: z.string().min(1).regex(regex.name, {
     message: "Invalid app name. Must be a valid npm package name.",
   }),
   tailwind: z.boolean(),
   eslint: z.boolean(),
   app: z.boolean(),
   srcDir: z.boolean(),
-  importAlias: z.string().regex(regex.importAlias, {
+  importAlias: z.string().min(1).regex(regex.importAlias, {
     message: "Invalid import alias. Must be a valid import alias.",
   }),
   lang: z.union([z.literal("ts"), z.literal("js")]),
